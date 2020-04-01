f32b72af3f8bdd454197b2883bd132339bc48e59
jose <nhkcm@hotmail.com>
2020-04-01 18:29:07 -0500

    reto 2

---
+++ diponibilidad/noiseInductor/noiseInductor/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Timers;
+
+namespace noiseInductor
+{
+    class Program
+    {
+        static Timer timer;
+        static Random rnd;
+        static int[] sampleScale = new int[] { 2, 5, 7 };
+        static int sampleIndex = 0;
+        static void Main(string[] args)
+        {
+            rnd = new Random();
+            timer = new Timer();
+            timer.Interval = 60 * 1000;
+            timer.Elapsed += new ElapsedEventHandler(TimerEventProcessor);
+            timer.Start();
+            Console.ReadLine();
+        }
+
+        static List<int> RandomSample(int sample)
+        {
+            rnd = new Random();
+            var response = new HashSet<int>();
+
+            while (response.Count < sample)
+            {
+                response.Add(rnd.Next(0, 10));
+            }
+
+            return response.ToList();
+        }
+
+        public static void TimerEventProcessor(object sender, ElapsedEventArgs e)
+        {
+            var pods = GetPodsNames("soilder").ToArray();
+
+            if (sampleIndex > 2) return;
+
+            var scale = sampleScale[sampleIndex];
+
+            var sample = RandomSample(scale);
+
+            System.Threading.Interlocked.Increment(ref sampleIndex);
+
+            sample.AsParallel().ForAll(i =>
+            {
+                DeletePod(pods[i]);
+            });
+
+            Console.WriteLine("Destroy Event with shutdown " + scale + " instances at");
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine();
+        }
+
+        private static void DeletePod(string v)
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+
+            // open it
+
+            runspace.Open();
+
+            // create a pipeline and feed it the script text
+
+            Pipeline pipeline = runspace.CreatePipeline();
+
+            pipeline.Commands.AddScript("kubectl delete pod " + v);
+            pipeline.Commands.Add("Out-String");
+
+            Collection<PSObject> results = pipeline.Invoke();
+
+            runspace.Close();
+        }
+
+        static List<String> GetPodsNames(string prefix)
+        {
+
+            var response = new List<String>();
+
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+
+            // open it
+
+            runspace.Open();
+
+            // create a pipeline and feed it the script text
+
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript("kubectl get pods");
+
+            pipeline.Commands.Add("Out-String");
+
+            Collection<PSObject> results = pipeline.Invoke();
+
+            // close the runspace
+
+            runspace.Close();
+
+            // convert the script result into a single string
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                response.AddRange(
+                    obj.ToString()
+                    .Split('\n')
+                    .Where(w => w.StartsWith(prefix))
+                    .Select(s => s.Split(' ')[0])
+                    );
+            }
+
+            return response;
+        }
+    }
+}
---
+++ diponibilidad/noiseInductor/parseCsvs/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace parseCsvs
+{
+    class TableResume
+    {
+
+    }
+    class pair
+    {
+        public string Time { get; set; }
+        public int CountOk { get; set; }
+        public int CountError { get; set; }
+        public double Latency { get; set; }
+    }
+    class Program
+    {
+        static string csv1 = "D:/results/results_test_100_per_second.csv";
+        static string csv2 = "D:/results/results_test_200_per_second.csv";
+        static string csv3 = "D:/results/results_test_300_per_second.csv";
+
+        static void Main(string[] args)
+        {
+            using var reader = new StreamReader(csv3);
+            var buffer = new StringBuilder();
+
+            var a = new StringBuilder();
+            var b = new StringBuilder();
+
+            var listA = new List<KeyValuePair<string, string>>();
+            var listB = new List<KeyValuePair<string, string>>();
+            var listC = new List<KeyValuePair<string, string>>();
+
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+
+                if (values[4].Contains("OK"))
+                {
+                    listA.Add(new KeyValuePair<string, string>(values[0], values[4]));
+                }
+                else
+                {
+                    listB.Add(new KeyValuePair<string, string>(values[0], values[4]));
+                }
+
+                listC.Add(new KeyValuePair<string, string>(values[0], values[14]));
+            }
+
+            var a1 = listA.GroupBy(g => g.Key).Select(s => new pair()
+            {
+                Time = s.Key,
+                CountOk = s.Count()
+            }).ToList();
+
+            var b1 = listB.GroupBy(g => g.Key).Select(s => new pair()
+            {
+                Time = s.Key,
+                CountError = s.Count()
+            }).ToList();
+
+            var c1 = listC.GroupBy(g => g.Key).Select(s => new pair()
+            {
+                Time = s.Key,
+                Latency = s.Select(t => Convert.ToInt32(t.Value)).Average()
+            }).ToList();
+
+            a1.AddRange(b1);
+            a1.AddRange(c1);
+
+            var f = a1.GroupBy(g => g.Time).Select(s => new pair()
+            {
+                Time = s.Key,
+                CountOk = s.Sum(t => t.CountOk),
+                CountError = s.Sum(t => t.CountError),
+                Latency = s.Max(m => m.Latency)
+            }).OrderBy(o => o.Time)
+              .Select(s => s.Time + "\t" + s.CountOk + "\t" + s.CountError + "\t" + Convert.ToInt32(s.Latency))
+              .ToList();
+
+            var fa1 = string.Join("\n", f).Replace("2020/03/28", "2020/03/18");
+
+        }
+    }
+}
